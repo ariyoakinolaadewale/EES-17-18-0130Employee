dcfc10e3c9042028b1fb777f2e3036f079cd96ec
ariyoakinolaadewale <78347388+ariyoakinolaadewale@users.noreply.github.com>
2021-03-14 13:49:21 -0700

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        private decimal salary;
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+
+
+
+        public Employee (string firstName, string lastName, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0)
+                    salary = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employ1 = new Employee("Akin", "Wale", 2000.50M);
+            Employee employ2 = new Employee("Florence", "Ariyo", 500.25M);
+
+            Console.WriteLine("Employee 1 First Name: {0}", employ1.FirstName);
+            Console.WriteLine("Employee 1 Last Name: {0}", employ1.LastName);
+            Console.WriteLine("Employee 1 Salary: {0:C}", employ1.Salary);
+
+            Console.WriteLine("Employee 2 First Name: {0}", employ2.FirstName);
+            Console.WriteLine("Employee 2 Last Name: {0}", employ2.LastName);
+            Console.WriteLine("Employee 2 Salary: {0:C}", employ2.Salary);
+
+            Console.WriteLine();
+            Console.WriteLine("After 10% raise");
+
+            employ1.Salary = employ1.Salary * 1.1M;
+            employ2.Salary = employ2.Salary * 1.1M;
+            Console.WriteLine("Employee 1 new salary: {0:C}", employ1.Salary);
+            Console.WriteLine("Employee 2 new salary: {0:C}", employ2.Salary);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
